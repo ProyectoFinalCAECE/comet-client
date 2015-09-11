70533d9bff8e8f9ea315de1168f1ee80cc7381f1
marianoquevedo <marianorquevedo@gmail.com>
2015-09-11 01:56:30 -0300

    Version preliminar de Crear proyecto

--- app/src/projects/project-create.controller.js
+++ app/src/projects/project-create.controller.js
@@ -9,26 +9,53 @@
     angular.module('cometApp')
            .controller('ProjectCreateController', ProjectCreateController);
 
-        ProjectCreateController.$inject = ['$rootScope', '$state', 'ngToast', 'projectService'];
+        ProjectCreateController.$inject = ['$rootScope', '$state', '$timeout', 'ngToast', 'projectService'];
 
-        function ProjectCreateController ($rootScope, $state, ngToast, projectService) {
+        function ProjectCreateController ($rootScope, $state, $timeout, ngToast, projectService) {
 
           var vm = this;
-          vm.project = null;
-          vm.created = false;
+          vm.project = {};
+          vm.invites = [];
           vm.create = create;
+          vm.addInvite = addInvite;
+          vm.removeInvite = removeInvite;
+          vm.validationErrors = null;
 
           activate();
 
           /**
            * @name activate
            * @desc controller activation logic
-           */
+          */
           function activate () {
 
           }
 
+          /**
+           * @name addInvite
+           * @desc adds an invite to the list of invites
+          */
+          function addInvite() {
+            vm.invites.push({ address: 'ejemplo@gmail.com'});
+          }
+
+          /**
+           * @name removeInvite
+           * @desc removes an invite to the list of invites
+          */
+          function removeInvite(item) {
+            var index = Array.indexOf(item);
+            if (index > -1) {
+                vm.invites.splice(index, 1);
+            }
+          }
+
+          /**
+           * @name create
+           * @desc project creation logic
+          */
           function create () {
+            vm.project.members = vm.invites;
             projectService.create(vm.project).error(function(data) {
               vm.validationErrors = $rootScope.helpers.loadServerErrors(data);
               if (vm.validationErrors === null)
@@ -36,7 +63,10 @@
                 ngToast.danger('Ocurrió un error al consultar al servidor.');
               }
             }).then(function() {
-                $state.go('home');
+                ngToast.success("El proyecto ha sido creado exitosamente.");
+                $timeout( function () {
+                  $state.go('dashboard.project-list');
+                }, 1000);
             });
           }
         }
