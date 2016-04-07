37990e28c30f60f5ff23445506993778a9419364
marianoquevedo <marianorquevedo@gmail.com>
2016-04-07 01:44:07 -0300

    Calls - Guardo, actualizo members y summary

--- app/src/services/call.service.js
+++ app/src/services/call.service.js
@@ -16,15 +16,17 @@
         return {
             create: create,
             update: update,
-            getById: getById
+            getById: getById,
+            updateMembers: updateMembers,
+            updateSummary: updateSummary
         };
 
         /**
          * @name create
          * @desc creates a new call
          */
-        function create (projectId, call) {
-          var url = getBaseUrl(projectId);
+        function create (projectId, channelId, call) {
+          var url = getBaseUrl(projectId, channelId);
           return $http.post(url, call, authService.getJwtHeader());
         }
 
@@ -32,26 +34,45 @@
          * @name getById
          * @desc returns a call by id
          */
-        function getById (projectId, callId) {
-          var url = getBaseUrl(projectId) + callId;
+        function getById (projectId, channelId, callId) {
+          var url = getBaseUrl(projectId, channelId) + callId;
           return $http.get(url, authService.getJwtHeader());
         }
 
         /**
          * @name update
-         * @desc update call 
+         * @desc update call
          */
-        function update (projectId, call) {
-          var url = getBaseUrl(projectId) + call.id;
+        function update (projectId, channelId, call) {
+          var url = getBaseUrl(projectId, channelId) + call.id;
           return $http.put(url, call, authService.getJwtHeader());
+        }
+
+        /**
+         * @name updateMembers
+         * @desc update call members
+         */
+        function updateMembers (projectId, channelId, callId) {
+          var url = getBaseUrl(projectId, channelId) + callId + '/members';
+          return $http.put(url, { test : '' }, authService.getJwtHeader());
+        }
+
+        /**
+         * @name updateSummary
+         * @desc update call summary
+         */
+        function updateSummary (projectId, channelId, call) {
+          var url = getBaseUrl(projectId, channelId) + call.id + '/summary';
+          return $http.put(url, { summary : call.summary }, authService.getJwtHeader());
         }
 
         /**
          * @name getbaseUrl
          * @desc return the base url for the channels resource
          */
-        function getBaseUrl(projectId) {
-          return parentUrl + projectId + resourceUrl;
+        function getBaseUrl(projectId, channelId) {
+          //'/projects/:project_id/channels/:channel_id/calls'
+          return parentUrl + projectId + '/channels/' + channelId + resourceUrl;
         }
     }
 })();
