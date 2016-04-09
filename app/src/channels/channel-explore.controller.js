ff40520629d71ba15db76d6815994d85fc424717
marianoquevedo <marianorquevedo@gmail.com>
2016-04-09 12:49:07 -0300

    Videollamada - Mensaje de summary

--- app/src/channels/channel-explore.controller.js
+++ app/src/channels/channel-explore.controller.js
@@ -433,12 +433,15 @@
            * Compares two messages and returns the order which they should be displayed in.
            */
           function compare(a,b) {
-            if (a.date < b.date)
+            if (a.date < b.date) {
               return -1;
-            else if (a.date > b.date)
+            }
+            else if (a.date > b.date) {
               return 1;
-            else
+            }
+            else {
               return 0;
+            }
           }
 
           /**
@@ -890,7 +893,7 @@
           function startCall() {
               var roomId = $rootScope.helpers.randomString(10).toUpperCase(),
                   newCall = {},
-                  channelId;
+                  channelId = getChannelRoomId();
 
               newCall.startHour = new Date();
               newCall.frontend_id = roomId;
@@ -910,10 +913,10 @@
                                       callId: response.data.id,
                                       room: roomId
                                     });
+                sendMessage(callUrl, user.id, messageType.CALL, callUrl);
                 showSummary(newCall);
-                $window.open(callUrl);
+                //$window.open(callUrl);
               });
-              //sendMessage(callUrl, user.id, messageType.CALL, callUrl);
           }
 
           /**
@@ -947,9 +950,8 @@
              var channelId = vm.channel.id;
              callService.updateSummary(vm.project.id, channelId, call).then(function (response) {
                console.log('update summary', response);
+               sendMessage(summary, user.id, messageType.CALL_SUMMARY);
              });
-             // TODO: send an auto generated message
-             //sendMessage(summary, user.id, messageType.AUTO);
            });
           }
       }
